05ce630c2e510a24425a7df553134bdf93df0c8c
Padmja Salvi <142106551+Padmja26@users.noreply.github.com>
2024-03-24 22:40:32 +0530

    Add files via upload

---
+++ exam/about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Developer Portfolio</title>
+    <link rel="stylesheet" type="text/css" href="css\styles.css">
+</head>
+<body>
+    <header>
+        <h1>Developer Portfolio</h1>
+    </header>
+    <div class="container">
+        <h2>About Us</h2>
+        <p>We are students currently studying at Pandit Deendayal Energy University. We are enthusiatic coders,who love web developing.</p>
+
+        <h2>Our Work</h2>
+        <p>We have several projects in our hand. You can find our work on GitHub:<a href = "https://github.com/padmja26">https://github.com/padmja26</a></p>
+
+        <h2>Skills</h2>
+        <p>We both have profound skills in php,css and javascript.</p>
+
+        <h2>Contact</h2>
+        <p>You can reach us at <a href="mailto:padmjasalvi5@gmail.com">padmjasalvi5@gmail.com</a></p>
+    </div>
+</body>
+</html>
